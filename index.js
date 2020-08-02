209be64ac89b1694fe18fe4c08b4ed1011544970
digzom <dicksonmelo@gmail.com>
2020-08-02 18:29:02 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+function createPhoneNumber(numbers){
+    let cleaned  = ('' + numbers).replace(/\D/g, '')
+    var match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/)
+    if (match) {
+      return '(' + match[1] + ') ' + match[2] + '-' + match[3]
+    }
+    return null
+}
